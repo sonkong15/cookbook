0745d7f788d4746b66bbb8a19d198e2e25659c19
donnell rawlings <donnierawlings@gmail.com>
2014-08-06 21:00:03 -0400

    first commit cps :x

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+	def home
+		
+	end
+	def terms
+		
+	end
+	def about
+		
+	end
+	def privacy
+		
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cps::Application.config.secret_token = 'e4fc3e1a17a52bc233a06ddf6363a4bf74fcae4e53b3968b8dc090eede316f6cfa9e796b18cdf7c506942759c5a221eb4cac3494b1954c8b84828f083bdc84d5'
